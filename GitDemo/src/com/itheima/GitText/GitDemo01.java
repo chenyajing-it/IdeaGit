9c4d3fbd7ef94f84f495ab08bd533b4b8402ea44
chenyajing <1305549726@qq.com>
2021-05-18 20:00:05 +0800

    version2

--- GitDemo/src/com/itheima/GitText/GitDemo01.java
+++ GitDemo/src/com/itheima/GitText/GitDemo01.java
@@ -3,5 +3,6 @@ package com.itheima.GitText;
 public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("I like itcast");
+        System.out.println("I like itcast");
     }
 }
